45a84d9e790eb5e9259ca9689f70bb5cf459eeb0
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-13 00:00:12 -0500

    Login y Register Corregidos

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('reading-lists/', views.all_reading_list, name='all_reading_list'),
--- src/users/views.py
+++ src/users/views.py
@@ -16,7 +16,7 @@ def register(request):
     else:
         form = LibraryUserCreationForm()
 
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 
 
 @login_required
